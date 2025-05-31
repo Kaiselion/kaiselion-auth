d258aa1def6a9177661789eefb7e207ac6426115
Kaiselion <github.com@kaisel.cc>
2025-05-31 01:49:04 -0400

    Refactoriza el middleware de autenticación para manejar errores y simplificar la lógica de sesión. Se eliminan rutas de autenticación innecesarias y se mejora la gestión de usuarios y sesiones en el contexto.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,27 +2,23 @@ import { auth } from '@/lib/auth'
 import { defineMiddleware } from 'astro:middleware'
 
 export const onRequest = defineMiddleware(async (context, next) => {
-  const authPaths = ['/dashboard']
-  const noAuthPaths = ['/', '/login', '/register', 'forgotpassword']
+  try {
+    const isAuthed = await auth.api.getSession({
+      headers: context.request.headers
+    })
 
-  const isAuthed = await auth.api.getSession({
-    headers: context.request.headers
-  })
+    if (isAuthed) {
+      context.locals.user = isAuthed.user
+      context.locals.session = isAuthed.session
+    } else {
+      context.locals.user = null
+      context.locals.session = null
+    }
 
-  if (noAuthPaths.includes(context.url.pathname) && isAuthed) {
     return next()
+  } catch (error) {
+    console.error('Error en middleware de autenticación:', error)
+    // Manejar el error de forma más elegante
+    return new Response('Error de autenticación', { status: 500 })
   }
-  if (authPaths.includes(context.url.pathname) && !isAuthed) {
-    return context.redirect('/login')
-  }
-
-  if (isAuthed) {
-    context.locals.user = isAuthed.user
-    context.locals.session = isAuthed.session
-  } else {
-    context.locals.user = null
-    context.locals.session = null
-  }
-
-  return next()
 })
